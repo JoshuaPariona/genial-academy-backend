94b8b6c4334be942e808a0d0609536ebef09696f
JoshiParSa <joshiparsa@gmail.com>
2024-10-11 20:22:38 -0500

    last update

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,53 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { AuthUserRouter } from "./routers/AuthUserRouter";
+import { AcademyRouter } from "./routers/AcademyRouter";
 import { AppConfig } from "./config/AppConfig";
-import { DataSource } from "typeorm";
+import { Logger } from "./utils/Logger";
+import { AppDataSource } from "./database/AppDataSource";
 
-class AppInitializer extends AppConfig {
-  public app: express.Application = express();
-  private port: number = this.getEnvNumber("PORT");
+export class App extends AppConfig {
+  private readonly logger: Logger = new Logger("App");
+  private readonly app: express.Application = express();
+  private readonly port: number = this.getEnvNumber("PORT");
 
   constructor() {
     super();
-    this.init();
-    this.dbConnection();
   }
 
   private setMiddlewares() {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(
-      morgan(
-        "[API] :method :url :status :res[content-length] - :response-time ms"
-      )
-    );
+    this.app.use(morgan(this.morganFormat));
     this.app.use(cors());
   }
 
   private setRouters() {
-    this.app.use("/api", [new AuthUserRouter().router]);
+    this.app.use("/api", new AcademyRouter().router);
   }
 
-  private async dbConnection(): Promise<void> {
-    try {
-      const dataSource = new DataSource(this.typeORMConfig);
-      await dataSource.initialize();
-      console.info("[Database] Database Connected");
-
-      this.setMiddlewares();
-      this.setRouters();
-
-      this.app.listen(this.port, () => {
-        console.info(`[Express] App running on http://localhost:${this.port}`);
-      });
-    } catch (error) {
-      console.error(`[Database] Database Connection Error: ${error}`);
-      console.info(
-        "[Express] Shutting down genial-academy-backend express application"
-      );
-    }
-  }
-
-  public init() {
-    console.info(
-      "[Express] Starting genial-academy-backend express application"
+  public start() {
+    this.logger.info(
+      "Starting genial-academy-backend express application",
+      "Express"
     );
+    AppDataSource.initialize(this.typeORMConfig)
+      .then(() => {
+        this.setMiddlewares();
+        this.setRouters();
+        this.app.listen(this.port, () => {
+          this.logger.info(
+            "App running on",
+            "Express",
+            `http://localhost:${this.port}`
+          );
+        });
+      })
+      .catch(() => {
+        this.logger.info(
+          "Shutting down genial-academy-backend express application",
+          "Express"
+        );
+      });
   }
 }
-
-new AppInitializer();
--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import morgan from "morgan";
 import { DataSourceOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
@@ -37,9 +38,32 @@ export abstract class AppConfig {
       database: this.getEnvString("DB_DATABASE"),
       entities: [__dirname + "../../entities/*{.ts,.js}"],
       migrations: [__dirname + "../../migrations/*{.ts,.js}"],
-      synchronize: true,
+      synchronize: true, //false
+      //migrationsRun: true,
       logging: false,
-      namingStrategy: new SnakeNamingStrategy()
+      namingStrategy: new SnakeNamingStrategy(),
     };
+  }
+
+  public get morganFormat(): string {
+    const getColor = (status: number): number => {
+      if (status >= 500) {
+        return 31;
+      } else if (status >= 400) {
+        return 33;
+      } else if (status >= 300) {
+        return 36;
+      } else if (status >= 200) {
+        return 32;
+      } else {
+        return 31;
+      }
+    };
+    morgan.token("statusColor", (_, res, __) => {
+      const status = res.statusCode;
+      const color = getColor(status);
+      return "\x1b[" + color + "m" + status + "\x1b[0m";
+    });
+    return "\x1b[32m[Api] \x1b[35m<:method>\x1b[0m :statusColor :url :response-time ms length=>:res[content-length]";
   }
 }
