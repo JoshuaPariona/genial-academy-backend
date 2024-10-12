0238733886ada50160f43ca9692c8bc1bebb3691
JoshiParSa <joshiparsa@gmail.com>
2024-10-11 20:19:26 -0500

    Main router Middlewar

--- src/middlewares/AcademyMiddleware.ts
+++ src/middlewares/AcademyMiddleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
-export class AuthUserMiddleware {
-  public getAuthUser(req: Request, res: Response) {
+export class AcademyMiddleware {
+  public getUniversityById(req: Request, res: Response) {
     res.status(200).json({
       email: "joshua@gmail.com",
     });
