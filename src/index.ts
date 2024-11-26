3f9eefef0da23d03c7d2e6b434036f8eec839cfa
JoshuaPariona <joshiparsa@gmail.com>
2024-11-26 03:47:43 -0500

    Creating folder main and test, Moving all src children to main

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { App } from "./App";
+import { App } from "./main/App";
 
 const app = new App();
 app.start();
---
+++ src/test/index.ts
@@ -0,0 +1,5 @@
+class Test {
+  constructor(parameters: any) {
+    console.log(parameters)
+  }
+}
