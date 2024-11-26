a95212185402c7d8774bde37b908b2c21af5497c
JoshuaPariona <joshiparsa@gmail.com>
2024-11-26 04:55:22 -0500

    Addig jest config file with ts-jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  //roots: ["<rootDir>/src/test"],
+  //moduleNameMapper: {
+  //  "^@main/(.*)$": "<rootDir>/src/main/$1",
+  //},
+  //moduleFileExtensions: ["ts", "js", "json", "node"],
+
+  //testMatch: ["**/*.test.ts", "**/*.spec.ts"],
+  //collectCoverage: true,
+  //collectCoverageFrom: ["src/main/**/*.{ts,js}", "!src/main/**/*.d.ts"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+};
