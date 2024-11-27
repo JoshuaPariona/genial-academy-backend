f07272517b0511168488da9ce3a9f8446f0c7fc2
JoshuaPariona <joshiparsa@gmail.com>
2024-11-27 04:49:00 -0500

    Creating authentication service method to save a new user and encrypting their password

--- src/main/auth/services/AuthService.ts
+++ src/main/auth/services/AuthService.ts
@@ -4,6 +4,7 @@ import { AuthUserEntity } from "../../entities/AuthUserEntity";
 import { AuthUserService } from "../../services/AuthUserService";
 import { AppConfig } from "../../config/AppConfig";
 import { JwtJson, JwtPayload } from "../interfaces";
+import { SignUpUserDTO } from "../validations/dto/SignUpUserDTO";
 
 export class AuthService extends AuthUserService {
   constructor() {
@@ -23,6 +24,16 @@ export class AuthService extends AuthUserService {
         return authUser;
     }
     return null;
+  }
+
+  public async saveAuthUserWithUser(
+    signUpDto: SignUpUserDTO
+  ): Promise<AuthUserEntity> {
+    const passwordHash = await bcrypt.hash(signUpDto.password, 10);
+    return await this.save({
+      ...signUpDto,
+      passwordHash,
+    });
   }
 
   public async generateJwt(authUser: AuthUserEntity): Promise<JwtJson> {
