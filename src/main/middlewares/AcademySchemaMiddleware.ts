02d3744c580dfdf3d3f1304c2a17b1a6c99bcd36
JoshuaPariona <joshiparsa@gmail.com>
2024-11-27 04:41:25 -0500

    Using the new objetive middleware for authenticate

--- src/main/middlewares/AcademySchemaMiddleware.ts
+++ src/main/middlewares/AcademySchemaMiddleware.ts
@@ -1,15 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthMiddleware } from "../auth/middlewares/AuthMiddleware";
-import { JwtStrategy } from "../auth/strategies/JwtStrategy";
 
 export class AcademySchemaMiddleware extends AuthMiddleware {
   public get base(): Array<
     (req: Request, res: Response, next: NextFunction) => void
   > {
     return [
-      this.authenticate(JwtStrategy.stgName),
+      this.authenticateWithJwt,
       this.validateIsAuthenticated,
-      this.validateAuthUserEntity,
     ];
   }
 }
