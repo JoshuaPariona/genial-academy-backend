fdd268aed79d582e774f9c5abbcb47ae6dc7b649
JoshuaPariona <joshiparsa@gmail.com>
2024-11-27 04:47:12 -0500

    Implementing user registration logic in the controller's sigup method

--- src/main/auth/controllers/AuthController.ts
+++ src/main/auth/controllers/AuthController.ts
@@ -3,6 +3,7 @@ import { Responser } from "../../utils/Responser";
 import { Logger, LogLevel } from "../../utils/Logger";
 import { AuthService } from "../services/AuthService";
 import { AuthUserEntity } from "../../entities/AuthUserEntity";
+import { SignUpUserDTO } from "../validations/dto/SignUpUserDTO";
 
 export class AuthController {
   private readonly authService: AuthService = new AuthService();
@@ -15,7 +16,7 @@ export class AuthController {
         const authUser = req.user as AuthUserEntity;
         const jwtJson = await this.authService.generateJwt(authUser);
         res.header("Content-Type", "application/json");
-        res.cookie("accessToken", jwtJson.accessToken, {maxAge: 60000 * 60});
+        res.cookie("accessToken", jwtJson.accessToken, { maxAge: 60000 * 60 });
         Responser.OK(res, jwtJson, "Autorizado");
       } catch (error) {
         if (error instanceof Error) {
@@ -71,37 +72,30 @@ export class AuthController {
   public get signUp(): (req: Request, res: Response) => Promise<void> {
     return async (req: Request, res: Response) => {
       try {
-        /*
-        const body = req.body as SignUpUserDTO;
-        const [isEmailUsed, isUsernameUsed] = await Promise.all([
-          this.authUserService.isEmailUsed(body.email),
-          this.authUserService.isUsernameUsed(body.username),
-        ]);
+        const signUpDto = req.body as SignUpUserDTO;
 
+        const [isEmailUsed, isUsernameUsed] = await Promise.all([
+          this.authService.isEmailUsed(signUpDto.email),
+          this.authService.isUsernameUsed(signUpDto.username),
+        ]);
         if (isEmailUsed || isUsernameUsed) {
+          const msg = isEmailUsed
+            ? "correo electrónico"
+            : "nombre de usuario";
           Responser.BAD_REQUEST(
             res,
-            isEmailUsed
-              ? "El correo electrónico esta actualmente en uso 😅, intenta con otro"
-              : "El nombre de usuario esta actualmente en uso 😅, intenta con otro"
+            `El ${msg} esta actualmente en uso 😅, intenta con otro`
           );
         } else {
-          const passwordHash = await bcrypt.hash(body.password, 10);
-          const publicEmail = body.email;
-          const user = this.userService.create({ ...body, publicEmail });
-          const authUser = this.authUserService.create({
-            ...body,
-            passwordHash,
-          });
-          authUser.user = user;
-          await this.authUserService.save(authUser);
-
+          const authUser = await this.authService.saveAuthUserWithUser(
+            signUpDto
+          );
           Responser.OK(
             res,
-            `Registro exitoso, Como estas ${body.names} ${body.surNames} 🚀`
+            authUser,
+            `Registro exitoso, Como estas ${authUser.user.names} ${authUser.user.surNames} 🚀`
           );
         }
-          */
       } catch (error) {
         if (error instanceof Error) {
           Logger.log({
