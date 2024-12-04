f338be82235d6b20b28b41048beafb22c3cf2a95
JoshuaPariona <joshiparsa@gmail.com>
2024-12-04 01:28:18 -0500

    Adding reporters config

--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,17 @@ module.exports = {
   //  "^@main/(.*)$": "<rootDir>/src/main/$1",
   //},
   //moduleFileExtensions: ["ts", "js", "json", "node"],
-
-  
+  reporters: [
+    "default",
+    [
+      "jest-html-reporter",
+      { outputPath: "docs/reports/html/test-report.html" },
+    ],
+    [
+      "jest-junit",
+      { outputDirectory: "docs/reports/junit", outputName: "report.xml" },
+    ],
+  ],
   testMatch: ["**/*.test.ts", "**/*.spec.ts"],
   //collectCoverage: true,
   //collectCoverageFrom: ["src/main/**/*.{ts,js}", "!src/main/**/*.d.ts"],
